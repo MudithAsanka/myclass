43740151692755ade3ea028733ef8b2b54c8b5b9
unknown <mudithasanka7@gmail.com>
2020-01-16 01:56:00 +0530

    about us dummy code fixed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -28,7 +28,7 @@
             <b>Who we are</b>
             <p>Though we established recently, as a secondary educational institute catering to the young individuals waiting to contribute to the growth of the country, “My Class” is perhaps the most common name that comes to the minds of both parents and students in search of assistance in studies.<br>
                 Over the years our institute has produced some of the top scoring students in the Island in the Science, Mathematics, Commerce, Technology and Art streams who not only have the subject knowledge but also are talented in applying it in real life situations.<br>
-                By being true to our mission statement “To guide our students towards the fortunes of knowledge producing winners amongst winners as well as disciplined all-round individuals with respectable social values” we have grown over the years as group of companies located in the Galle area and aim to achieve more together with our panel of lecturers.afnjkajsdkasgdagasjddhgasdhajashdgdjahsg<br>
+                By being true to our mission statement “To guide our students towards the fortunes of knowledge producing winners amongst winners as well as disciplined all-round individuals with respectable social values” we have grown over the years as group of companies located in the Galle area and aim to achieve more together with our panel of lecturers.<br>
                 My Class Higher Education Institute is the only organization to recognize and celebrate the achievements of their best performers.</p>
         </div>
     </div>
